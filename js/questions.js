0c7f16fffd36e7c9eefd1235738b78e7bdb74800
AD1025 <adarshika2502@gmail.com>
2022-12-22 01:35:36 +0530

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "OS computer abbreviation usually means?",
+    answer: "Operating System",
+    options: [
+      "Order of Significance",
+      "Open Software",
+      "Operating System",
+      "Optical Sensor"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: ".MOV extension refers usually to what kind of file?",
+    answer: "Animation file",
+    options: [
+      "Image file",
+      "Animation file",
+      "Audio file",
+      "MS Office document"
+    ]
+  },
+    {
+    numb: 4,
+    question: "DB computer abbreviation means?",
+    answer: "Database",
+    options: [
+      "Double type",
+      "Data block",
+      "Driver boot",
+      "Database"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+];
